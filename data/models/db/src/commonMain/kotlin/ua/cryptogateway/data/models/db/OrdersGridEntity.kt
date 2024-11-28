b3349aa4039a94eb58787bc7aaf627acee2ef788
Evgeniy-Khl <graviton.ua@gmail.com>
2024-11-28 08:11:06 +0200

    modified data class OrdersGridEntity

--- data/models/db/src/commonMain/kotlin/ua/cryptogateway/data/models/db/OrdersGridEntity.kt
+++ data/models/db/src/commonMain/kotlin/ua/cryptogateway/data/models/db/OrdersGridEntity.kt
@@ -1,6 +1,26 @@
 package ua.cryptogateway.data.models.db
 
+import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class OrdersGridEntity(val pairname: String?, val side: String?)
+//data class OrdersGridEntity(val pairname: String?, val side: String?)
+data class OrdersGridEntity(val pairname: String?,
+                            val side: String?,
+                            val remvolume: Double?,
+                            val volume: Double?,
+                            val pricebid: Double?,
+                            val price: Double?,
+                            val priceask: Double?,
+                            val change: Float?,
+                            val gecko: Double?,
+                            val offset: Float?,
+                            val smashort: Double?,
+                            val smalong: Double?,
+                            val atr: Int,
+                            val id: String?,
+                            val status: String?,
+                            val ordtype: String?,
+                            val deleteord: Boolean?,
+                            val timemarket: LocalDateTime?,
+                            val lost: Boolean?)
