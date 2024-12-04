737151e3e8c996086b09b3e291371a9f341965b4
Evgeniy-Khl <graviton.ua@gmail.com>
2024-12-04 10:24:27 +0200

    Add object "ActiveSchema"

---
+++ data/db/src/commonMain/kotlin/ua/cryptogateway/data/db/schema/ActiveSchema.kt
@@ -0,0 +1,33 @@
+package ua.cryptogateway.data.db.schema
+
+import org.jetbrains.exposed.sql.Table
+
+//{
+//    "id": "5992a049-8612-409d-8599-2c3d7298b106",            // Unique identifier of an order
+//    "type": "Limit",                                         // Type of an order
+//    "quantity": "5",                                         // Original order quantity
+//    "executedQuantity": "0",                                 // Traded quantity in stock (>0 if traded)
+//    "cumulativeQuoteQty": "0",                               // Traded quantity in money (>0 if traded)
+//    "cost": "0.05",                                          // Total amount
+//    "side": "Bid",                                           // Bid for buying base asset, Ask for selling base asset. FYI: For BTC_USDT trading pair, BTC is the base asset
+//    "pair": "TRX_USDT",                                      // Traded pair
+//    "price": "0.01",                                         // Price of the trade
+//    "status": "Open",                                        // The status of the order
+//    "createdAt": "2023-07-11T07:04:20.131Z",                 // Date-time of order creation, UTC
+//    "updatedAt": "2023-07-11T07:04:20.131Z"                  // Date-time of the last update of the order, UTC
+//}
+
+object ActiveSchema : Table("dbo.active") {
+    val id = varchar("id", 36)
+    val type = varchar("type", 12)
+    val quantity = integer("quantity")
+    val executedQuantity = integer("executedQuantity")
+    val cumulativeQuoteQty = integer("cumulativeQuoteQty")
+    val cost = double("cost")
+    val side = varchar("side",5)
+    val pair = varchar("pair",15)
+    val price = double("price")
+    val status = varchar("status",12)
+    val createdAt = varchar("createdAt",25)
+    val updatedAt = varchar("updatedAt",25)
+}
