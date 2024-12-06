a4f91ed98813f9f55e55b9dfcb4696295ad7fcc1
Evgeniy-Khl <graviton.ua@gmail.com>
2024-12-06 07:03:18 +0200

    Update "timestamp"

--- data/db/src/commonMain/kotlin/ua/cryptogateway/data/db/models/BalanceEntity.kt
+++ data/db/src/commonMain/kotlin/ua/cryptogateway/data/db/models/BalanceEntity.kt
@@ -1,11 +1,14 @@
 package ua.cryptogateway.data.db.models
 
-import kotlinx.datetime.Instant
+import kotlinx.datetime.*
 
 data class BalanceEntity(
     val currency: String,
     val balance: Double,
     val lockBalance: Double,
     val entire: Double,
-    val timestamp: Instant,
-)
+    val timestamp: Instant = Clock.System.now(),
+) {
+    val localTimeStamp: LocalDateTime
+        get() = timestamp.toLocalDateTime(TimeZone.currentSystemDefault())
+}
