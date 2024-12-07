ad5ea78c5491b352b65501dc7bf90f35e5348179
Evgeniy-Khl <graviton.ua@gmail.com>
2024-12-07 16:08:36 +0200

    Remove unused classes

--- domain/src/commonMain/kotlin/ua/cryptogateway/domain/DomainComponent.kt
+++ domain/src/commonMain/kotlin/ua/cryptogateway/domain/DomainComponent.kt
@@ -1,11 +1,9 @@
 package ua.cryptogateway.domain
 
-import ua.cryptogateway.domain.observers.ObserveMe
 import ua.cryptogateway.domain.services.BalancePullService
 import ua.cryptogateway.domain.services.TickersPullService
 
 interface DomainComponent {
-    val observeMe: ObserveMe    // TODO: Remove after testing
     val balancePuller: BalancePullService
     val tickersPuller: TickersPullService
 }
