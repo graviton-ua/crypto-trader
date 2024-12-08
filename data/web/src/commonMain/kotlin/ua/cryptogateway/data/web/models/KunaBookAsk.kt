82daf9d0fb73f743f4889d34afcc2a0be4d572fe
Evgeniy-Khl <graviton.ua@gmail.com>
2024-12-08 09:02:29 +0200

    Add data class "KunaBookAsk", data class "KunaBookBid"

---
+++ data/web/src/commonMain/kotlin/ua/cryptogateway/data/web/models/KunaBookAsk.kt
@@ -0,0 +1,35 @@
+package ua.cryptogateway.data.web.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+//{
+//    "asks": [        // An array of sell orders
+//    [
+//        "16950",     // Sell price, level 1
+//        "0.001"      // Sell quantity, level 1
+//    ],
+//    [
+//        "17000",     // Sell price, level 2
+//        "0.01"       // Sell quantity, level 2
+//    ]
+//    ],
+//    "bids": [        // An array of buy orders
+//    [
+//        "16700",     // Sell price, level 1
+//        "0.01"       // Sell quantity, level 1
+//    ],
+//    [
+//        "16000",     // Sell price, level 2
+//        "0.001"      // Sell quantity, level 2
+//    ]
+//    ]
+//}
+
+@Serializable
+data class KunaBookAsk(
+    @SerialName("sellPrice") val price: Double,
+    @SerialName("volume") val volume: Double,
+    @SerialName("sumVolume") val sumVolume: Double
+)
